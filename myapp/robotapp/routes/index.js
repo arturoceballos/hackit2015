5367ec9c53cabf153d1a5f154a41f4ea38ce986a
vanie3 <vansing3@gmail.com>
2015-05-02 18:09:47 -0700

    version 1.16.2 of rethinkdb

--- myapp/robotapp/routes/index.js
+++ myapp/robotapp/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var r = require('rethinkdb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/submit', function(req, res) {
+  var connection = null;
+  r.connect( {host: '127.0.0.1', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+  });
+  console.log(req.body);
+  r.table('contact_form').insert(req.body);
+  res.redirect('/thanks');
+});
+
+router.get('/thanks', function(req, res) {
+  res.render('thanks');
 });
 
 module.exports = router;
