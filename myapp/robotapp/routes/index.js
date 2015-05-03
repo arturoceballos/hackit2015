b72bab65cb8d88b33b6be0c894f3d53f7dc6ac1a
vanie3 <vansing3@gmail.com>
2015-05-02 18:48:38 -0700

    form input sent to database

--- myapp/robotapp/routes/index.js
+++ myapp/robotapp/routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var r = require('rethinkdb');
+var connection = null;
+r.connect( {host: '127.0.0.1', port: 28015}, function(err, conn) {
+  if (err) throw err;
+  connection = conn;
+});
+
+var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,13 +21,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/submit', function(req, res) {
-  var connection = null;
-  r.connect( {host: '127.0.0.1', port: 28015}, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-  });
   console.log(req.body);
-  r.table('contact_form').insert(req.body);
+  r.db('hackit_db').table('contact_form').insert(req.body).run(connection);
   res.redirect('/thanks');
 });
 
